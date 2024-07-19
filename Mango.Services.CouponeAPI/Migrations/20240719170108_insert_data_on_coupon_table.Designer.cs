325dea57362b21c51d259af452aa937e9f71b338
shahazan shahid <shahazan.shahid@gmail.com>
2024-07-20 01:02:12 +0800

    add data on coupon table

--- Mango.Services.CouponeAPI/Migrations/20240719170108_insert_data_on_coupon_table.Designer.cs
+++ Mango.Services.CouponeAPI/Migrations/20240719170108_insert_data_on_coupon_table.Designer.cs
@@ -3,6 +3,7 @@ using Mango.Services.CouponeAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Mango.Services.CouponeAPI.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240719170108_insert_data_on_coupon_table")]
+    partial class insert_data_on_coupon_table
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
